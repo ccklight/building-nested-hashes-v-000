a3aadff0145c4ba2e5a90429171b862bc99b8a1b
Cheryl K <cherylkelson@icloud.com>
2018-09-21 16:06:43 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,13 +8,11 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {name: "Lady Capulet" age: "51"}, 
-      :matriarch => {
-       
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => {name: "Lord Capulet" age: "50"}, 
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"}
+      
+    
       :heroine_friends => []
    }
   }
